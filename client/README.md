0ae9d6b6fa7e32b9eab8b0d74bfa8711a2f4017d
tanaliga100 <jordantanaliga@gmail.com>
2023-01-27 08:20:50 +0800

    first commit

---
+++ client/README.md
@@ -0,0 +1,5 @@
+#### Proxy
+
+<!-- "proxy": "https://user-workflow-11.herokuapp.com" -->
+
+"proxy": "http://localhost:5000"
---
+++ server/db/connect.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const connectDB = (url) => {
+  return mongoose.connect(url);
+};
+
+module.exports = connectDB;
